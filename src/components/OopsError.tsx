2341f8fc862841dea1dc7e3fc8582caa69853fc9
Yergeni Cabranes <yergenis@gmail.com>
2023-06-11 22:51:17 -0400

    feat: add error boundary

---
+++ src/components/OopsError.tsx
@@ -0,0 +1,13 @@
+
+export default function OopsError() {
+  
+  return (
+    <div role="alert" style={{ height: '100dvh', display: 'flex', alignItems: 'center' }}>
+      <div style={{ display: 'inline-block', verticalAlign: 'middle', margin: 'auto' }}>
+        <div style={{ textAlign: 'center', paddingLeft: '8px', paddingRight: '8px' }}>
+          <h1>Oops! Something went wrong</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
